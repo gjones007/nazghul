79f593602943737d42eef71dfbb1e9d8e9225c59
Gordon McNutt <gmcnutt@cableone.net>
2013-07-13 16:58:31 -0600

    Added locations.scm to store locations that need to be referenced from multiple places. Fixed some ladders from dungeons back to the overworld.

---
+++ worlds/haxima-1.002/locations.scm
@@ -0,0 +1,12 @@
+;; Locations referred to more than once
+
+(define bandit-hideout-loc (list 'p_shard 364 421))
+(define angriss-lair-loc (list 'p_shard 417 365))
+(define the-mans-hideout-loc (list 'p_shard 273 270))
+(define brundegardt-loc (list 'p_shard 357 302))
+(define lost-halls-loc (list 'p_shard 315 494))
+(define kurpolis-loc (list 'p_shard 240 233))
+(define mushroom-cave-loc (list 'p_shard 343 455))
+(define necromancers-lair-loc (list 'p_shard 299 486))
+(define smoldering-cave-loc (list 'p_shard 521 66))
+(define slimy-cavern-loc (list 'p_shard 198 228))
