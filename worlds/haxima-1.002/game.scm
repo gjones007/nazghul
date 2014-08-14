d592ed07db68da95d7cec2fb07d1cdc81efc313a
Gordon McNutt <gmcnutt@cableone.net>
2014-08-13 22:15:17 -0600

    Moved layer definitions to layers.scm

--- worlds/haxima-1.002/game.scm
+++ worlds/haxima-1.002/game.scm
@@ -243,19 +243,7 @@
 (define faction-player-outlaw 14)
 (define faction-num           15)
 
-;; Layers (must match object.h)
-(define layer-none       0)
-(define layer-tfeat      1)
-(define layer-mechanism  2)
-(define layer-portal     3)
-(define layer-vehicle    4)
-(define layer-bed        5)
-(define layer-container  6)
-(define layer-item       7)
-(define layer-field      8)
-(define layer-being      9)
-(define layer-projectile 10)
-(define layer-crosshair  11)
+(load "layers.scm")
 
 ;; Contexts
 (define context-world 1)
