7fd6ce2904a652918efbc2f0430f4781d5278751
Gordon McNutt <gmcnutt@cableone.net>
2014-09-05 08:55:26 -0600

    Modified Gregor's conv to use the new macros.

--- worlds/haxima-1.002/gregor.scm
+++ worlds/haxima-1.002/gregor.scm
@@ -67,48 +67,13 @@
 	  )))
 
 (define (gregor-camp knpc kpc)
-  (say knpc "Use the 'k' key to ^c+bkamp^c-, err...camp, in the wilderness and heal up."))
-
-(define (gregor-dang knpc kpc)
-  (say knpc 
-       "Very dangerous! If you need healing, a town inn is the safest place."
-       " You can camp in the wilderness but it's dangerous when you're alone and have no one to keep watch. "
-       " Of course, there are spells and potions for healing, too.")
-  )
+  (say knpc "Use the 'k' key to kamp, err...camp, in the wilderness and heal up."))
 
 (define (gregor-dead knpc kpc)
   (say knpc "Aye, it's a shame. My daughter and her husband were both killed by trogs."))
 
-(define (gregor-charcoal knpc kpc)
-  (say knpc "I take charcoal into town and sell it, and some folks come by my place to buy it."))
-
 (define (gregor-hut knpc kpc)
   (say knpc "My hut's in the forest to the South and East. Just myself and my granddaughter living there now."))
-
-(define (gregor-ench knpc kpc)
-  (quest-data-assign-once 'questentry-calltoarms)
-  (say knpc 
-       "The Enchanter is one of the Wise."
-       " He told me to look out for a Wanderer like you."
-       " If I saw one I was to send him his way. "
-       " He lives in the White Tower.")
-  (quest-wise-subinit 'questentry-enchanter)
-  (quest-data-update 'questentry-enchanter 'common 1)
-  )
-
-(define (gregor-cave knpc kpc)
-  (say knpc "There, that little trail that leads off the main path to the South and West. "
-       "Follow it on in. Open the chest. Get the stuff. "
-       "Come back and we'll talk again, if you have more questions."))
-
-(define (gregor-ches knpc kpc)
-  (say knpc "Go ahead and open it and get the stuff inside."))
-
-(define (gregor-stuf knpc kpc)
-  (say knpc "The common folk made offerings in the cave, thinking one day a Wanderer might come again."))
-
-(define (gregor-leav knpc kpc)
-  (say knpc "If you want to leave just follow the trail south and step off the map."))
 
 (define (gregor-band knpc kpc)
   (let ((quest (gregor-quest (kobj-gob-data knpc))))
@@ -145,131 +110,139 @@
                   (kern-conv-end)
                   ))))))
 
-(define (gregor-bye knpc kpc)
-  (let ((quest (gregor-quest (kobj-gob-data knpc))))
-    (cond ((quest-accepted? quest)
-           (say knpc "Farewell, and be careful."))
-          (else
-           (say knpc "Wait! Before you go, I have a favor to ask you.")
-           (prompt-for-key)
-           (gregor-band knpc kpc)
-           ))))
-
 (define gregor-conv
   (ifc basic-conv
-       ;; default if the only "keyword" which may (indeed must!) be longer than
-       ;; 4 characters. The 4-char limit arises from the kernel's practice of
-       ;; truncating all player queries to the first four characters. Default,
-       ;; on the other hand, is a feature of the ifc mechanism (see ifc.scm).
-       (method 'default (lambda (knpc kpc) (say knpc "Can't help you there.")))
-       (method 'hail gregor-hail)
-       (method 'health (lambda (knpc kpc) (say knpc "[cough] Well enough, my granddaughter helps take care of me.")))
-       (method 'bye gregor-bye)
-       (method 'job (lambda (knpc kpc) (say knpc "I'm a charcoal burner. I also care for this shrine.")))
-       (method 'join (lambda (knpc kpc) (say knpc "Nope. Already got a job.")))
-       (method 'name (lambda (knpc kpc) (say knpc "Gregor's my name.")))
 
-       (method 'cave gregor-cave)
-       (method 'ches gregor-ches)
+       (reply 'default "Can't help you there.")
 
-       (method 'stuf gregor-stuf)
-       (method 'equi gregor-stuf)  ;; A synonym
-       (method 'gear gregor-stuf)  ;; A synonym
+       (react 'hail
+	      (if (in-player-party? 'ch_nate)
+		  (gregor-kill-nate knpc kpc)
+		  (if (in-inventory? kpc t_letter_from_enchanter)
+		      (say knpc 
+			   "I see you got your stuff, and that letter from the Enchanter."
+			   " Don't forget to ready your weapons before leaving."
+			   " It's dangerous out there!")
+		      (say knpc
+			   "Welcome, Wanderer. I've been watching for you."
+			   " There's some things that belong to you, over in yonder cave."
+			   " Go in where the chest is, open it, and get the things inside."
+			   " It's all for you.")
+		      )))
+       (reply 'health "[cough] Well enough, my granddaughter helps take care of me.")
 
-       (method 'dang gregor-dang)
+       (react 'bye
+	      (let ((quest (gregor-quest (kobj-gob-data knpc))))
+		(cond ((quest-accepted? quest)
+		       (say knpc "Farewell, and be careful."))
+		      (else
+		       (say knpc "Wait! Before you go, I have a favor to ask you.")
+		       (prompt-for-key)
+		       (gregor-band knpc kpc)
+		       ))))
+
+       (reply 'job "I'm a charcoal burner. I also care for this shrine.")
+       (reply 'join "Nope. Already got a job.")
+       (reply 'name "Gregor's my name.")
+       (reply 'cave "There, that little trail that leads off the main path to the South and West."
+	      " Follow it on in. Open the chest. Get the stuff. "
+	      " Come back and we'll talk again, if you have more questions.")
+       (reply 'ches "Go ahead and open it and get the stuff inside.")
+       (reply 'stuf "The common folk made offerings in the cave, thinking one day a Wanderer might come again.")
+
+       (reply 'dang "Very dangerous! If you need healing, a town inn is the safest place."
+	      " You can camp in the wilderness but it's dangerous when you're alone and have no one to keep watch. "
+	      " Of course, there are spells and potions for healing, too.")
 
        (method 'camp gregor-camp)
        (method 'kamp gregor-camp) ;; A synonym
 
        (method 'band gregor-band)
-       (method 'leav gregor-leav)
 
-       (method 'char gregor-charcoal)
-       (method 'burn gregor-charcoal)  ;; A synonym
-
-       (method 'daug (lambda (knpc kpc) (say knpc "Aye, she was a near-witch like her mother. "
-                                             "Had the knack, but not enough to be among the Wise.")))
+       (reply 'leav "If you want to leave just follow the trail south and step off the map.")
+       (reply 'char "I take charcoal into town and sell it, and some folks come by my place to buy it.")
+       (reply 'daug "Aye, she was a near-witch like her mother."
+	      " Had the knack, but not enough to be among the Wise.")
        (method 'dead gregor-dead)
-       (method 'ench gregor-ench)
-       (method 'folk (lambda (knpc kpc) (say knpc "There's homesteads scattered about in "
-                                             "the woods and the foothills.")))
-       (method 'fore (lambda (knpc kpc) (say knpc "Stay out of the deep woods. "
-                                             "Bandits, spiders and worse live there.")))
-       (method 'gate (lambda (knpc kpc) (say knpc "No one can predict when it will open, "
+
+       (react 'ench 
+	      (quest-data-assign-once 'questentry-calltoarms)
+	      (say knpc "The Enchanter is one of the Wise."
+		   " He told me to look out for a Wanderer like you."
+		   " If I saw one I was to send him his way. "
+		   " He lives in the White Tower.")
+	      (quest-wise-subinit 'questentry-enchanter)
+	      (quest-data-update 'questentry-enchanter 'common 1))
+
+       (reply 'folk "There's homesteads scattered about in "
+	      "the woods and the foothills.")
+       (reply 'fore "Stay out of the deep woods. "
+	      "Bandits, spiders and worse live there.")
+       (reply 'gate "No one can predict when it will open, "
                                              "or if anything will come through if it does. "
                                              "I've heard of other gates in other parts of the land, "
-                                             "and stories tell of others long forgotten now.")))
-       (method 'gran (lambda (knpc kpc) (say knpc "I've a granddaughter name of Ilya.")))
-       (method 'help (lambda (knpc kpc) (say knpc "There's always folks who need help. "
-                                             "These are hard times in a hard land.")))
-       (method 'hill (lambda (knpc kpc) (say knpc "Trogs are always a threat in the foothills, "
-                                             "but more so of late.")))
-       (method 'husb (lambda (knpc kpc) (say knpc "My son-in-law was a simple farmer. "
+                                             "and stories tell of others long forgotten now.")
+       (reply 'gran "I've a granddaughter name of Ilya.")
+       (reply 'help "There's always folks who need help. "
+                                             "These are hard times in a hard land.")
+       (reply 'hill "Trogs are always a threat in the foothills, "
+                                             "but more so of late.")
+       (reply 'husb "My son-in-law was a simple farmer. "
                                              "Why the trogs attacked I don't know. "
                                              "Maybe they were driven out of the hills "
-                                             "by something else.")))
+                                             "by something else.")
        (method 'hut gregor-hut)
-       (method 'ilya (lambda (knpc kpc) (say knpc "Yep. She lives at my place now "
-                                             "that her parents are dead.")))
+       (reply 'ilya "Yep. She lives at my place now that her parents are dead.")
 
-       (method 'offe (lambda (knpc kpc) (say knpc "There in the cave you'll find a chest. "
-                                             "Take what's inside. Wanderers enter this world with little, "
-                                             "and in the past some have done great good, "
-                                             "so folks leave stuff in good will for the next one.")
-                             (quest-data-update 'questentry-whereami 'wanderer 2)
-                             ))
+       (react 'offe 
+	      (say knpc "There in the cave you'll find a chest. "
+		   "Take what's inside. Wanderers enter this world with little, "
+		   "and in the past some have done great good, "
+		   "so folks leave stuff in good will for the next one.")
+	      (quest-data-update 'questentry-whereami 'wanderer 2))
+
        (method 'pare gregor-dead)
        (method 'plac gregor-hut)
 
-       (method 'shar (lambda (knpc kpc) (say knpc "The Shard?  That's what we call this land, Wanderer.")
-                             (quest-data-update 'questentry-whereami 'shard 1)
-                             ))
+       (react 'shar 
+	      (say knpc "The Shard?  That's what we call this land, Wanderer.")
+	      (quest-data-update 'questentry-whereami 'shard 1))
 
-       (method 'shri (lambda (knpc kpc) (say knpc "This shrine is for those who come through the gate. "
-                                             "Wanderers like yourself. "
-                                             "Folks leave simple offerings here to help you on "
-                                             "your journey.")
-                             (quest-data-update 'questentry-whereami 'wanderer 1)
-                             ))
+       (react 'shri
+	      (say knpc "This shrine is for those who come through the gate. "
+		   "Wanderers like yourself. "
+		   "Folks leave simple offerings here to help you on "
+		   "your journey.")
+	      (quest-data-update 'questentry-whereami 'wanderer 1))
 
-       (method 'spid (lambda (knpc kpc)
-		       (say knpc
-			    "Some spiders in the deep woods are monstrous -- big as oxen!"
-			    " Children of Angriss, we call those.")
-		       ))
-       (method 'angr (lambda (knpc kpc)
-		       (say knpc
-			    "Angriss, Mother of all wood spiders."
-			    " She's just a legend to scare the kids to keep them out of the woods.")
-		       ))
+       (reply 'spid
+	      "Some spiders in the deep woods are monstrous -- big as oxen!"
+	      " Children of Angriss, we call those.")
+       (reply 'angr
+	      "Angriss, Mother of all wood spiders."
+	      " She's just a legend to scare the kids to keep them out of the woods.")
+       (reply 'trog
+	      "Trogs eat us, if they can."
+	      " Even crack the bones and suck the marrow."
+	      " Nothing left to bury.")
+       (react 'wand 
+	      (say knpc "We call those who come through the gate Wanderers. "
+		   "No one knows where they come from or where they go. "
+		   "You are the first to come through in a long, long time.")
+	      (quest-data-update 'questentry-whereami 'wanderer 1))
+       (reply 'wise
+	      "The Wise are both strong and -mostly- good."
+	      " They help the land, as they can, "
+	      " and keep the Accursed at bay.")
+       (reply 'accu
+	      "The Accursed? People say they trade their souls for power."
+	      " If not for the Wise they would overrun the Shard.")     
        
-       (method 'trog (lambda (knpc kpc) (say knpc "Trogs eat us, if they can. "
-					     "Even crack the bones and suck the marrow. "
-					     "Nothing left to bury.")))
-
-       (method 'wand (lambda (knpc kpc) (say knpc "We call those who come through the gate Wanderers. "
-                                             "No one knows where they come from or where they go. "
-                                             "You are the first to come through in a long, long time.")
-                             (quest-data-update 'questentry-whereami 'wanderer 1)
-                             ))
-
-       (method 'wise (lambda (knpc kpc) (say knpc "The Wise are both strong and -mostly- good. "
-                                             "They help the land, as they can, "
-					     "and keep the Accursed at bay.")))
-       (method
-        'accu
-        (lambda (knpc kpc) 
-          (say knpc "The Accursed? People say they trade their souls for power. "
-               "If not for the Wise they would overrun the Shard.")))     
-       
-       (method 'witc
-	       (lambda (knpc kpc)
-		 (say knpc "Don't know of any witches in these parts any more, unless you count Hackle.")))
-       (method 'hack
-	       (lambda (knpc kpc)
-		 (say knpc "Hackle is a mad old woman who lives in Bole."
-		      " Some say she's a witch."
-		      " Just crazy, I say.")))
+       (reply 'witc "Don't know of any witches in these parts any more, unless you count Hackle.")
+       (reply 'hack
+	      "Hackle is a mad old woman who lives in Bole."
+	      " Some say she's a witch."
+	      " Just crazy, I say.")
        ))
 
 (define (mk-gregor)
