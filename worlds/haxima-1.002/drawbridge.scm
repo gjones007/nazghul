07662789c871c2f5e92534b885b6388c31fef064
Gordon McNutt <gmcnutt@cableone.net>
2015-01-30 20:21:10 -0700

    Fixed drawbridge to show the right sprite in north/south direction.

--- worlds/haxima-1.002/drawbridge.scm
+++ worlds/haxima-1.002/drawbridge.scm
@@ -1,28 +1,36 @@
-;; Accessors - a drawbridge gob is simply its sprite in the 'on state
-(define (drawbridge-sprite-tag drawbridge) 
-  (display "drawbridge-sprite-tag drawbridge:")(display drawbridge)(newline)
-  drawbridge)
+;; A drawbridge terrain feature.
+;;
+;; A drawbridge is meant to be controlled by a lever or similar mechanism. When
+;; turned "on", it looks and is passable like a bridge. When "off" it is
+;; impassable and invisible.
+;;
+;; Example:
+;;
+;;  (put (kern-tag 'my-drawbridge (mk-drawbridge north)) 32 33)
+;;  (put (mk-lever 'my-drawbridge) 33 34)
 
-;; Extend the bim interface to support the 'signal message
+
+;; Define a state change handler
 (define (drawbridge-state on? kobj)
   (let* ((bim (gob-data (kobj-gob kobj))))
     (if on?
-        (state-mk (drawbridge-sprite-tag (bim-members bim)) #f pclass-bridge 0)
+        (state-mk (bim-members bim) #f pclass-bridge 0)
         (state-mk nil #f pclass-none   0))))
 
+;; Extend the bim interface and override the state handler
 (define drawbridge-ifc
   (ifc bim-ifc
        (method 'signal bim-toggle)
        (method 'state drawbridge-state)
        ))
 
-;; Make a kernel drawbridge type
+;; Make a kernel type
 (mk-obj-type 'TF_drawbridge "drawbridge" nil layer-tfeat drawbridge-ifc)
 
 ;; Define a constructor
 (define (mk-drawbridge dir)
-  (case dir
-    ((north south) (bind (kern-mk-obj TF_drawbridge 1) 
-                         (bim-mk #f '() 's_ns_bridge)))
-    (else          (bind (kern-mk-obj TF_drawbridge 1) 
-                         (bim-mk #f '() 's_ew_bridge)))))
+  (if (or (eqv? dir north) (eqv? dir south))
+     (bind (kern-mk-obj TF_drawbridge 1)
+	   (bim-mk #f '() 's_ns_bridge))
+     (bind (kern-mk-obj TF_drawbridge 1)
+	   (bim-mk #f '() 's_ew_bridge))))
